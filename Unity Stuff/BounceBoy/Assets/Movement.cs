c6a2d54f57ad42e3f6659221517cc800b348cbb6
CrystalThistle <g.sickenga@gmail.com>
2016-07-15 17:13:36 +0200

    BOUNCY BOI

---
+++ Unity Stuff/BounceBoy/Assets/Movement.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Movement : MonoBehaviour
+{
+    public float xPos;
+    public float yPos;
+    public float xVel;
+    public float yVel;
+    public float gravity;
+
+    private float prevXPos;
+    private float prevYPos;
+    private float xSize;
+    private float ySize;
+    private float maxYpos;
+
+    private PixelPerfectCamera gameCamera;
+
+    void Start()
+    {
+        gameCamera = GameObject.FindGameObjectWithTag("MainCamera").GetComponent<PixelPerfectCamera>();
+
+        xSize = GetComponent<SpriteRenderer>().sprite.bounds.size.x;
+        ySize = GetComponent<SpriteRenderer>().sprite.bounds.size.y;
+
+        xPos = GetComponent<Transform>().transform.position.x;
+        yPos = GetComponent<Transform>().transform.position.y;
+
+        maxYpos = gameCamera.nativeResolution.y / 2;
+    }
+
+    void Update()
+    {
+        xPos += xVel;
+        yPos += yVel;
+
+        if (yVel > -10)
+        {
+            yVel -= gravity / 100;
+        }
+
+        if (yPos <= -maxYpos + ySize / 2 && yVel < 0)
+        {
+            yVel *= -1;
+            //yPos = -maxYpos + ySize / 2;
+        }
+
+        transform.position = new Vector3(xPos, yPos, 0);
+
+        prevXPos = xPos;
+        prevYPos = yPos;
+    }
+}
