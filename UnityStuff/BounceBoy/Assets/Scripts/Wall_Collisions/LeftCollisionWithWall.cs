2ecb04537410766a8fa85072ca0a9743a6c6cf5e
Matthijs Blankevoort <Matthijsblankevoort@hotmail.com>
2016-07-16 19:17:26 +0200

    ja

---
+++ UnityStuff/BounceBoy/Assets/Scripts/Wall_Collisions/LeftCollisionWithWall.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LeftCollisionWithWall : MonoBehaviour
+{
+    private BoxCollider2D collisionBounds;
+    private Movement playerMovement;
+
+    void Start()
+    {
+        collisionBounds = GetComponent<BoxCollider2D>();
+        playerMovement = GetComponentInParent<Movement>();
+    }
+
+	void Update()
+    {
+        collisionBounds.size = new Vector2(-playerMovement.xVel, collisionBounds.size.y);
+        collisionBounds.offset = new Vector2(-8 + playerMovement.xVel / 2f, collisionBounds.offset.y);
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        Vector2 colBlockSpriteBounds = other.GetComponent<SpriteRenderer>().sprite.bounds.size;
+
+        if (other.tag == "wall" && playerMovement.xVel < 0)
+        {
+            playerMovement.xVel *= -1;
+            playerMovement.xPos = other.transform.position.x + colBlockSpriteBounds.x / 2f + playerMovement.xSize / 2f;
+        }
+    }
+}
